99678e8edf07a6db84fc50c67d40e92db64d520b
Mohammed Osman <110602196+medo469@users.noreply.github.com>
2024-05-03 02:26:26 +0200

    Create summation in java.java

---
+++ programming 1/summation in java.java
@@ -0,0 +1,18 @@
+//summation in java
+package laps;
+import java.util.Scanner;
+public class lap3 {
+
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+    // TODO Auto-generated method stub
+System.out.println("please inter the sumation number")
+  int n =s.nextInt;
+	int sum=0;
+  for(int i=1;i<=n;i++) {
+	  sum=sum+i;
+	System.out.println(sum);
+  }
+	}
+
+}
