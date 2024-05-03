f5e4332b5d0d515cfd42c980897f34d0e702793d
Mohammed Osman <110602196+medo469@users.noreply.github.com>
2024-05-03 02:33:08 +0200

    Create cahrat in java.java

---
+++ programming 1/cahrat in java.java
@@ -0,0 +1,13 @@
+//cahrat in java
+package laps;
+import java.util.Scanner;
+public class lap3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String b=s.nextLine();
+  for(int i=0;i<b.length();i++) {
+System.out.println(b.charAt(i));
+  }
+	}
+}
