6b250b4ca34a5f42c277f1fb3f58a0698f4ffcd9
Mohammed Osman <110602196+medo469@users.noreply.github.com>
2024-05-03 02:45:18 +0200

    Create 2 dimensional array.java

---
+++ programming 1/2 dimensional array.java
@@ -0,0 +1,14 @@
+//2 dimensional array
+public class Lab3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubfor (int i=0,i<4,i++)
+{
+for (int j=0,j<4,j++)
+{
+if(i=j)
+c[i][j]=1;
+else
+c[i][j]=0;
+}
+}
+}}
